6a61bfa411ce47335af7b59723caa9ea0ff1e727
Farko Moot <farko_moot@hotmail.com>
2023-05-23 12:53:11 -0300

    modal fix

--- assets/index-47668b12.js
+++ assets/index-47668b12.js
@@ -7565,7 +7565,7 @@ const portifolio = "_portifolio_m51pf_58";
 const divform = "_divform_m51pf_73";
 const inputText = "_inputText_m51pf_93";
 const inputTextMsg = "_inputTextMsg_m51pf_104";
-const styles$6 = {
+const styles$7 = {
   inicio,
   contentText,
   contentImg,
@@ -7582,7 +7582,7 @@ const header_contentchildrens = "_header_contentchildrens_x7nwu_21";
 const header_icon = "_header_icon_x7nwu_39";
 const header_img = "_header_img_x7nwu_67";
 const header_buttons = "_header_buttons_x7nwu_79";
-const styles$5 = {
+const styles$6 = {
   header_content,
   header_contentchildrens,
   header_icon,
@@ -7602,9 +7602,9 @@ function Header() {
     var elmntToView3 = document.getElementById("contato");
     elmntToView3.scrollIntoView();
   }
-  return /* @__PURE__ */ jsx("div", { className: styles$5.header_content, children: /* @__PURE__ */ jsxs("div", { className: styles$5.header_contentchildrens, children: [
-    /* @__PURE__ */ jsx("div", { className: styles$5.header_icon, children: /* @__PURE__ */ jsx("p", { children: "GP" }) }),
-    /* @__PURE__ */ jsxs("div", { className: styles$5.header_buttons, children: [
+  return /* @__PURE__ */ jsx("div", { className: styles$6.header_content, children: /* @__PURE__ */ jsxs("div", { className: styles$6.header_contentchildrens, children: [
+    /* @__PURE__ */ jsx("div", { className: styles$6.header_icon, children: /* @__PURE__ */ jsx("p", { children: "GP" }) }),
+    /* @__PURE__ */ jsxs("div", { className: styles$6.header_buttons, children: [
       /* @__PURE__ */ jsx("p", { onClick: handleInicio, children: "Inicio" }),
       /* @__PURE__ */ jsx("p", { onClick: handlePortifolio, children: "Portofolio" }),
       /* @__PURE__ */ jsx("p", { onClick: handleContato, children: "Contato" })
@@ -7814,11 +7814,11 @@ var mdiLinkedin = "M19 3A2 2 0 0 1 21 5V19A2 2 0 0 1 19 21H5A2 2 0 0 1 3 19V5A2 
 var mdiNodejs = "M12,1.85C11.73,1.85 11.45,1.92 11.22,2.05L3.78,6.35C3.3,6.63 3,7.15 3,7.71V16.29C3,16.85 3.3,17.37 3.78,17.65L5.73,18.77C6.68,19.23 7,19.24 7.44,19.24C8.84,19.24 9.65,18.39 9.65,16.91V8.44C9.65,8.32 9.55,8.22 9.43,8.22H8.5C8.37,8.22 8.27,8.32 8.27,8.44V16.91C8.27,17.57 7.59,18.22 6.5,17.67L4.45,16.5C4.38,16.45 4.34,16.37 4.34,16.29V7.71C4.34,7.62 4.38,7.54 4.45,7.5L11.89,3.21C11.95,3.17 12.05,3.17 12.11,3.21L19.55,7.5C19.62,7.54 19.66,7.62 19.66,7.71V16.29C19.66,16.37 19.62,16.45 19.55,16.5L12.11,20.79C12.05,20.83 11.95,20.83 11.88,20.79L10,19.65C9.92,19.62 9.84,19.61 9.79,19.64C9.26,19.94 9.16,20 8.67,20.15C8.55,20.19 8.36,20.26 8.74,20.47L11.22,21.94C11.46,22.08 11.72,22.15 12,22.15C12.28,22.15 12.54,22.08 12.78,21.94L20.22,17.65C20.7,17.37 21,16.85 21,16.29V7.71C21,7.15 20.7,6.63 20.22,6.35L12.78,2.05C12.55,1.92 12.28,1.85 12,1.85M14,8C11.88,8 10.61,8.89 10.61,10.39C10.61,12 11.87,12.47 13.91,12.67C16.34,12.91 16.53,13.27 16.53,13.75C16.53,14.58 15.86,14.93 14.3,14.93C12.32,14.93 11.9,14.44 11.75,13.46C11.73,13.36 11.64,13.28 11.53,13.28H10.57C10.45,13.28 10.36,13.37 10.36,13.5C10.36,14.74 11.04,16.24 14.3,16.24C16.65,16.24 18,15.31 18,13.69C18,12.08 16.92,11.66 14.63,11.35C12.32,11.05 12.09,10.89 12.09,10.35C12.09,9.9 12.29,9.3 14,9.3C15.5,9.3 16.09,9.63 16.32,10.66C16.34,10.76 16.43,10.83 16.53,10.83H17.5C17.55,10.83 17.61,10.81 17.65,10.76C17.69,10.72 17.72,10.66 17.7,10.6C17.56,8.82 16.38,8 14,8Z";
 var mdiReact = "M12,10.11C13.03,10.11 13.87,10.95 13.87,12C13.87,13 13.03,13.85 12,13.85C10.97,13.85 10.13,13 10.13,12C10.13,10.95 10.97,10.11 12,10.11M7.37,20C8,20.38 9.38,19.8 10.97,18.3C10.45,17.71 9.94,17.07 9.46,16.4C8.64,16.32 7.83,16.2 7.06,16.04C6.55,18.18 6.74,19.65 7.37,20M8.08,14.26L7.79,13.75C7.68,14.04 7.57,14.33 7.5,14.61C7.77,14.67 8.07,14.72 8.38,14.77C8.28,14.6 8.18,14.43 8.08,14.26M14.62,13.5L15.43,12L14.62,10.5C14.32,9.97 14,9.5 13.71,9.03C13.17,9 12.6,9 12,9C11.4,9 10.83,9 10.29,9.03C10,9.5 9.68,9.97 9.38,10.5L8.57,12L9.38,13.5C9.68,14.03 10,14.5 10.29,14.97C10.83,15 11.4,15 12,15C12.6,15 13.17,15 13.71,14.97C14,14.5 14.32,14.03 14.62,13.5M12,6.78C11.81,7 11.61,7.23 11.41,7.5C11.61,7.5 11.8,7.5 12,7.5C12.2,7.5 12.39,7.5 12.59,7.5C12.39,7.23 12.19,7 12,6.78M12,17.22C12.19,17 12.39,16.77 12.59,16.5C12.39,16.5 12.2,16.5 12,16.5C11.8,16.5 11.61,16.5 11.41,16.5C11.61,16.77 11.81,17 12,17.22M16.62,4C16,3.62 14.62,4.2 13.03,5.7C13.55,6.29 14.06,6.93 14.54,7.6C15.36,7.68 16.17,7.8 16.94,7.96C17.45,5.82 17.26,4.35 16.62,4M15.92,9.74L16.21,10.25C16.32,9.96 16.43,9.67 16.5,9.39C16.23,9.33 15.93,9.28 15.62,9.23C15.72,9.4 15.82,9.57 15.92,9.74M17.37,2.69C18.84,3.53 19,5.74 18.38,8.32C20.92,9.07 22.75,10.31 22.75,12C22.75,13.69 20.92,14.93 18.38,15.68C19,18.26 18.84,20.47 17.37,21.31C15.91,22.15 13.92,21.19 12,19.36C10.08,21.19 8.09,22.15 6.62,21.31C5.16,20.47 5,18.26 5.62,15.68C3.08,14.93 1.25,13.69 1.25,12C1.25,10.31 3.08,9.07 5.62,8.32C5,5.74 5.16,3.53 6.62,2.69C8.09,1.85 10.08,2.81 12,4.64C13.92,2.81 15.91,1.85 17.37,2.69M17.08,12C17.42,12.75 17.72,13.5 17.97,14.26C20.07,13.63 21.25,12.73 21.25,12C21.25,11.27 20.07,10.37 17.97,9.74C17.72,10.5 17.42,11.25 17.08,12M6.92,12C6.58,11.25 6.28,10.5 6.03,9.74C3.93,10.37 2.75,11.27 2.75,12C2.75,12.73 3.93,13.63 6.03,14.26C6.28,13.5 6.58,12.75 6.92,12M15.92,14.26C15.82,14.43 15.72,14.6 15.62,14.77C15.93,14.72 16.23,14.67 16.5,14.61C16.43,14.33 16.32,14.04 16.21,13.75L15.92,14.26M13.03,18.3C14.62,19.8 16,20.38 16.62,20C17.26,19.65 17.45,18.18 16.94,16.04C16.17,16.2 15.36,16.32 14.54,16.4C14.06,17.07 13.55,17.71 13.03,18.3M8.08,9.74C8.18,9.57 8.28,9.4 8.38,9.23C8.07,9.28 7.77,9.33 7.5,9.39C7.57,9.67 7.68,9.96 7.79,10.25L8.08,9.74M10.97,5.7C9.38,4.2 8,3.62 7.37,4C6.74,4.35 6.55,5.82 7.06,7.96C7.83,7.8 8.64,7.68 9.46,7.6C9.94,6.93 10.45,6.29 10.97,5.7Z";
 const content$2 = "_content_1yzmd_1";
-const styles$4 = {
+const styles$5 = {
   content: content$2
 };
 function Skills() {
-  return /* @__PURE__ */ jsxs("div", { className: styles$4.content, children: [
+  return /* @__PURE__ */ jsxs("div", { className: styles$5.content, children: [
     /* @__PURE__ */ jsx(
       Icon.Icon,
       {
@@ -7862,11 +7862,11 @@ function Skills() {
   ] });
 }
 const content$1 = "_content_stwqe_1";
-const styles$3 = {
+const styles$4 = {
   content: content$1
 };
 function Footer() {
-  return /* @__PURE__ */ jsxs("div", { className: styles$3.content, children: [
+  return /* @__PURE__ */ jsxs("div", { className: styles$4.content, children: [
     /* @__PURE__ */ jsxs("div", { children: [
       /* @__PURE__ */ jsx("a", { href: "https://www.github.com/farkomoot", target: "_blank", children: /* @__PURE__ */ jsx(Icon.Icon, { path: mdiGithub, size: 2.5, color: "white" }) }),
       /* @__PURE__ */ jsx("a", { href: "https:///www.linkedin.com/in/farkomoot", target: "_blank", children: /* @__PURE__ */ jsx(Icon.Icon, { path: mdiLinkedin, size: 2.5, color: "white" }) })
@@ -7876,26 +7876,26 @@ function Footer() {
 }
 const card = "_card_14olr_1";
 const text = "_text_14olr_57";
-const styles$2 = {
+const styles$3 = {
   card,
   text
 };
 const btn = "_btn_1wvp6_1";
-const styles$1 = {
+const styles$2 = {
   btn
 };
 function Button(props) {
   if (props.textButton === "ENVIAR") {
-    return /* @__PURE__ */ jsx("button", { type: "submit", className: styles$1.btn, onClick: props.acao, children: /* @__PURE__ */ jsx("span", { children: props.textButton }) });
+    return /* @__PURE__ */ jsx("button", { type: "submit", className: styles$2.btn, onClick: props.acao, children: /* @__PURE__ */ jsx("span", { children: props.textButton }) });
   } else {
-    return /* @__PURE__ */ jsx("button", { type: "button", className: styles$1.btn, onClick: props.acao, children: /* @__PURE__ */ jsx("span", { children: props.textButton }) });
+    return /* @__PURE__ */ jsx("button", { type: "button", className: styles$2.btn, onClick: props.acao, children: /* @__PURE__ */ jsx("span", { children: props.textButton }) });
   }
 }
 const background = "_background_1tuhd_1";
 const content = "_content_1tuhd_27";
 const title = "_title_1tuhd_99";
 const paragrah = "_paragrah_1tuhd_115";
-const styles = {
+const styles$1 = {
   background,
   content,
   title,
@@ -7907,23 +7907,29 @@ function Modal(props) {
     window.open(link, "_blank");
   }
   if (props.modalIsOpen === true) {
-    return /* @__PURE__ */ jsx("section", { className: styles.background, children: /* @__PURE__ */ jsxs("section", { className: styles.content, children: [
-      /* @__PURE__ */ jsx("header", { children: /* @__PURE__ */ jsx("span", { onClick: props.modalClose, children: /* @__PURE__ */ jsx(
+    return /* @__PURE__ */ jsx("section", { className: styles$1.background, children: /* @__PURE__ */ jsxs("section", { className: styles$1.content, children: [
+      /* @__PURE__ */ jsx("header", { children: /* @__PURE__ */ jsx("span", { onClick: () => props.modalClose(), children: /* @__PURE__ */ jsx(
         Icon,
         {
           path: mdiCloseBoxOutline,
           size: 1.5
         }
       ) }) }),
-      /* @__PURE__ */ jsx("h2", { className: styles.title, children: props.name }),
+      /* @__PURE__ */ jsx("h2", { className: styles$1.title, children: props.name }),
       /* @__PURE__ */ jsx("img", { src: props.img }),
-      /* @__PURE__ */ jsx("p", { className: styles.paragrah, children: props.text }),
+      /* @__PURE__ */ jsx("p", { className: styles$1.paragrah, children: props.text }),
       /* @__PURE__ */ jsx(Button, { textButton: "Ver Mais", acao: handleOpenMoreDetials })
     ] }) });
   } else {
     return null;
   }
 }
+const modal_background = "_modal_background_hadjv_1";
+const modal_container = "_modal_container_hadjv_21";
+const styles = {
+  modal_background,
+  modal_container
+};
 function Cardprojeto(props) {
   const [isOpen, setIsOpen] = react.exports.useState(false);
   function openModal() {
@@ -7936,9 +7942,9 @@ function Cardprojeto(props) {
       setIsOpen(false);
     }
   }
-  return /* @__PURE__ */ jsxs("section", { className: styles$2.card, children: [
+  return /* @__PURE__ */ jsxs("section", { className: styles$3.card, children: [
     /* @__PURE__ */ jsx("img", { src: props.img }),
-    /* @__PURE__ */ jsx("p", { className: styles$2.text, children: props.name }),
+    /* @__PURE__ */ jsx("p", { className: styles$3.text, children: props.name }),
     /* @__PURE__ */ jsx("button", { onClick: openModal, children: /* @__PURE__ */ jsx("span", { children: "Ver Detalhes" }) }),
     /* @__PURE__ */ jsx(
       Modal,
@@ -7974,22 +7980,22 @@ function App() {
   }
   return /* @__PURE__ */ jsxs("div", { className: "App", children: [
     /* @__PURE__ */ jsx(Header, {}),
-    /* @__PURE__ */ jsxs("div", { className: styles$6.inicio, id: "inicio", children: [
-      /* @__PURE__ */ jsxs("div", { className: styles$6.contentText, children: [
-        /* @__PURE__ */ jsxs("p", { className: styles$6.title, children: [
+    /* @__PURE__ */ jsxs("div", { className: styles$7.inicio, id: "inicio", children: [
+      /* @__PURE__ */ jsxs("div", { className: styles$7.contentText, children: [
+        /* @__PURE__ */ jsxs("p", { className: styles$7.title, children: [
           "Olá, sou o ",
           /* @__PURE__ */ jsx("br", {}),
           "Gustavo Patrick"
         ] }),
-        /* @__PURE__ */ jsx("p", { className: styles$6.text, children: "Sou um entusiasta da Tecnologia e apaixonado por Esportes, Motos e Programação. Atualmente aspirante a desenvolvedor web e móvel com a tecnologia ReactJS, React Native e estudante do curso Interdisciplinar em Ciência e Tecnologia na UFVJM." }),
+        /* @__PURE__ */ jsx("p", { className: styles$7.text, children: "Sou um entusiasta da Tecnologia e apaixonado por Esportes, Motos e Programação. Atualmente aspirante a desenvolvedor web e móvel com a tecnologia ReactJS, React Native e estudante do curso Interdisciplinar em Ciência e Tecnologia na UFVJM." }),
         /* @__PURE__ */ jsx("div", { children: /* @__PURE__ */ jsx(Button, { textButton: "ENTRE EM CONTATO", click: "contato", acao: handleAction }) })
       ] }),
-      /* @__PURE__ */ jsx("div", { className: styles$6.contentImg, children: /* @__PURE__ */ jsx("img", { src: imgME }) })
+      /* @__PURE__ */ jsx("div", { className: styles$7.contentImg, children: /* @__PURE__ */ jsx("img", { src: imgME }) })
     ] }),
     /* @__PURE__ */ jsx(Skills, {}),
-    /* @__PURE__ */ jsxs("div", { className: styles$6.main, children: [
-      /* @__PURE__ */ jsxs("div", { className: styles$6.portifolio, id: "portifolio", children: [
-        /* @__PURE__ */ jsx("p", { className: styles$6.title, children: "PROJETOS RECENTES" }),
+    /* @__PURE__ */ jsxs("div", { className: styles$7.main, children: [
+      /* @__PURE__ */ jsxs("div", { className: styles$7.portifolio, id: "portifolio", children: [
+        /* @__PURE__ */ jsx("p", { className: styles$7.title, children: "PROJETOS RECENTES" }),
         /* @__PURE__ */ jsx("div", { children: objectCard.map((item) => {
           return /* @__PURE__ */ jsx(
             Cardprojeto,
@@ -8004,14 +8010,14 @@ function App() {
         }) }),
         /* @__PURE__ */ jsx(Button, { textButton: "VER MAIS", click: "portifolio", acao: handleAction2 })
       ] }),
-      /* @__PURE__ */ jsxs("div", { className: styles$6.divform, id: "contato", children: [
-        /* @__PURE__ */ jsx("p", { className: styles$6.title, children: "ENTRE EM CONTATO" }),
-        /* @__PURE__ */ jsxs("form", { action: "127.0.0.1:3000/sendmail", method: "POST", className: styles$6.form, children: [
+      /* @__PURE__ */ jsxs("div", { className: styles$7.divform, id: "contato", children: [
+        /* @__PURE__ */ jsx("p", { className: styles$7.title, children: "ENTRE EM CONTATO" }),
+        /* @__PURE__ */ jsxs("form", { action: "127.0.0.1:3000/sendmail", method: "POST", className: styles$7.form, children: [
           /* @__PURE__ */ jsx(
             "input",
             {
               id: "nome",
-              className: styles$6.inputText,
+              className: styles$7.inputText,
               type: "text",
               placeholder: "Nome",
               name: "nome",
@@ -8022,7 +8028,7 @@ function App() {
             "input",
             {
               id: "email_form",
-              className: styles$6.inputText,
+              className: styles$7.inputText,
               type: "email",
               placeholder: "E-mail",
               name: "email",
@@ -8033,7 +8039,7 @@ function App() {
             "input",
             {
               id: "telfone_form",
-              className: styles$6.inputText,
+              className: styles$7.inputText,
               type: "text",
               placeholder: "Telefone",
               name: "telefone",
@@ -8044,7 +8050,7 @@ function App() {
             "textarea",
             {
               id: "text_form",
-              className: styles$6.inputTextMsg,
+              className: styles$7.inputTextMsg,
               type: "textarea",
               placeholder: "Mensagem",
               name: "mensagem",
